1ba82da18b15f9c86e73f16de9f6c9c6f92018ce
Andrea Caneschi <128700479+CaneschiAndrea@users.noreply.github.com>
2024-02-26 11:24:54 +0100

    Update Alunno.php

--- Alunno.php
+++ Alunno.php
@@ -1,34 +1,44 @@
 <?php
-class Alunno{
+
+class Alunno implements JsonSerializable {
+
     protected $nome;
     protected $cognome;
     protected $eta;
 
-
     public function __construct($nome, $cognome, $eta){
-        $this->nome = $nome;
-        $this->cognome = $cognome;
-        $this->eta = $eta;
+        $this->nome=$nome;
+        $this->cognome=$cognome;
+        $this->eta=$eta;
+
     }
+
     public function getNome(){
         return $this->nome;
     }
+
     public function getCognome(){
         return $this->cognome;
     }
+
     public function getEta(){
         return $this->eta;
     }
-    public function setNome($nome){
-        $this->nome = $nome;
+
+    public function jsonSerialize() {
+        $attrs = [];
+        $class_vars = get_class_vars(get_class($this));
+        foreach ($class_vars as $name => $value) {
+          $attrs[$name]=$this->{$name};
+          
+         }
+        return $attrs;
     }
-    public function setCognome($cognome){
-        $this->cognome = $cognome;
-    }
-    public function setEta($eta){
-        $this->eta = $eta;
-    } public function toString(){
-        return "Nome: $this->nome" . "<br>" ."Cognome: $this->cognome" . "<br>" ."Età: $this->eta" . "<br><br>";
-    }
+    
+
+
+
 }
+
+
 ?>
