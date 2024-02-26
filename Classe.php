b9478778ba920630c7091c2e534ed648ff4ecad1
Andrea Caneschi <128700479+CaneschiAndrea@users.noreply.github.com>
2024-02-26 11:25:59 +0100

    Update Classe.php

--- Classe.php
+++ Classe.php
@@ -1,19 +1,47 @@
 <?php
-require("Alunno.php");
-class Classe{
-    protected $arrayy = [];
+require __DIR__ . '/Alunno.php';
+class Classe implements JsonSerializable {
+    protected $nome;
+    protected $array=[];
     public function __construct(){
+        $this->nome = "5B";
         $a1 = new Alunno("Steph", "Curry", 18);
         $a2 = new Alunno("Damian", "Lillard", 16);
         $a3 = new Alunno("Luka", "Doncic", 15);
-        $a4 = new Alunno("Lebron", "James", 19);
-        array_push($this->arrayy, $a1);
-        array_push($this->arrayy, $a2);
-        array_push($this->arrayy, $a3);
-        array_push($this->arrayy, $a4);
+        array_push($this->array, $a1);
+        array_push($this->array, $a2);
+        array_push($this->array, $a3);
+
     }
-   public function getArray(){
-    return $this->arrayy;
-   }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getArray(){
+        return $this->array;
+    }
+
+    public function getAlunnoByNome($nome){
+        foreach($this->array as $a){
+            if($nome == $a->getNome()){
+                return $a;
+            }
+        }
+        return null;
+    }
+
+    public function jsonSerialize() {
+        $attrs = [];
+        $class_vars = get_class_vars(get_class($this));
+        foreach ($class_vars as $name => $value) {
+          $attrs[$name]=$this->{$name};
+          
+         }
+        return $attrs;
+    }
+
+
 }
+
 ?>
